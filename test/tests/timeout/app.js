f5fbde9eed5c5f6eaea2e536656f8a2a38f5bf8a
Matthew Phillips <matthew@matthewphillips.info>
2017-12-13 11:07:29 -0500

    Upgrade to canjs 4.0

--- test/tests/timeout/app.js
+++ test/tests/timeout/app.js
@@ -1,26 +1,20 @@
-var Map = require("can-map");
-require("can-map-define");
+var DefineMap = require("can-define/map/map");
 var route = require("can-route");
 require("can-route-pushstate");
 
-module.exports = Map.extend({
-	define: {
-		page: {
-			type: "string"
-		},
+module.exports = DefineMap.extend({
+	page: "string",
+	race: {
+		get: function(last, resolve){
+			if(!last) {
+				var page = this.page;
+				var ms = page === "slow" ? 500 : 10;
 
-		race: {
-			get: function(last, set){
-				if(!last) {
-					var page = this.attr("page");
-					var ms = page === "slow" ? 500 : 10;
+				setTimeout(function(){
+					resolve("passed");
+				}, ms);
 
-					setTimeout(function(){
-						set("passed");
-					}, ms);
-
-					return "failed";
-				}
+				return "failed";
 			}
 		}
 	}
